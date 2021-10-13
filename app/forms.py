7d6eef67843ccccff9ac857f43abba5b12b96dcb
Taty <tatyanaparks@gmail.com>
2021-10-13 10:36:06 -0500

    fixed postform

--- app/forms.py
+++ app/forms.py
@@ -11,5 +11,5 @@ class UserInfoForm(FlaskForm):
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = StringField('Content', validators=[DataRequired(), Email()])
+    content = StringField('Content', validators=[DataRequired())
     submit = SubmitField()
